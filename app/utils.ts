23ec42bae276fce4e3cd1e7f7af139a1c679e2ff
David <dungreact@gmail.com>
2026-01-02 11:48:52 +0700

    Refactor getPokeData and getPikachuData to streamline return structure and ensure consistent error handling

--- app/utils.ts
+++ app/utils.ts
@@ -3,29 +3,32 @@ export interface Pokemon {
   id: number;
 }
 
-export const getPokeData = async (): Promise<{ name: string; time: string }> => {
-  const time = new Date().toLocaleTimeString();
-
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto', {
+export const getPokeData = async (): Promise<{
+  name: string;
+  time: string;
+}> => {
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/ditto", {
     next: { revalidate: 60 },
   });
-  
+
   if (!res.ok) {
-    throw new Error('Failed to fetch Pokemon data');
+    throw new Error("Failed to fetch Pokemon data");
   }
-  
-  return { ...res.json(), time };
+
+  return { ...res.json(), time: new Date().toLocaleTimeString() };
 };
 
-export const getPikachuData = async (): Promise<{ name: string; time: string }> => {
-  const time = new Date().toLocaleTimeString();
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu', {
+export const getPikachuData = async (): Promise<{
+  name: string;
+  time: string;
+}> => {
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu", {
     next: { revalidate: 60 },
   });
-  
+
   if (!res.ok) {
-    throw new Error('Failed to fetch Pokemon data');
+    throw new Error("Failed to fetch Pokemon data");
   }
-  
-  return { ...res.json(), time };
+
+  return { ...res.json(), time: new Date().toLocaleTimeString() };
 };
